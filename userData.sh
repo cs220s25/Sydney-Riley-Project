3095e160ec74dc0efb74e02758ce71e213cc0b6b
Sydney Shifman <shifmans@moravian.edu>
2025-04-15 11:25:33 -0400

    Changed to use Root instead of EC2 Instance

--- userData.sh
+++ userData.sh
@@ -16,10 +16,13 @@ yum install -y maven-amazon-corretto21
 yum install git -y
 
 # Clone the repository
-git clone https://github.com/cs220s25/Sydney-Riley-Project /home/ec2-user/Sydney-Riley-Project
+git clone https://github.com/cs220s25/Sydney-Riley-Project /Sydney-Riley-Project
 
 # Change to the project directory
-cd /home/ec2-user/Sydney-Riley-Project
+cd /Sydney-Riley-Project
+
+# Add execution permission for redeploy
+chmod +x redploy.sh
 
 # Clean and package project
 mvn clean package
