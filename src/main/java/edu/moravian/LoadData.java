9189b07cca6f5847518ddebad6acba0bc856d96a
rileyrivera <riverar02@moravian.edu>
2025-04-26 00:08:22 -0400

    edited LoadData.java so it connects to redis on docker

--- src/main/java/edu/moravian/LoadData.java
+++ src/main/java/edu/moravian/LoadData.java
@@ -15,32 +15,30 @@ public class LoadData
         return Files.readAllLines(filePath);
     }
 
-    public static void main(String[] args)
-    {
-        try
-        {
-            Redis redisActions = new Redis("localhost", 6379);
-            Lexicon lexiconActions = new RedisLexicon(new Jedis("localhost", 6379));
+    public static void main(String[] args) {
+        try {
+            String redisHost = System.getenv("REDIS_HOST");
+            int redisPort = Integer.parseInt(System.getenv("REDIS_PORT"));
+
+            Redis redisActions = new Redis(redisHost, redisPort);
+            Lexicon lexiconActions = new RedisLexicon(new Jedis(redisHost, redisPort));
 
             redisActions.resetToEmpty();
-
             redisActions.addCategory("animals", readFile("src/data/animals.txt"));
             System.out.println("animals added");
             redisActions.addCategory("cities", readFile("src/data/cities.txt"));
             System.out.println("cities added");
             redisActions.addCategory("demo", readFile("src/data/demo.txt"));
             System.out.println("demo added");
-
             lexiconActions.buildLexicon(readFile("src/data/animals.txt"));
             System.out.println("animals added to Redis");
             lexiconActions.buildLexicon(readFile("src/data/cities.txt"));
             System.out.println("cities added to Redis");
             lexiconActions.buildLexicon(readFile("src/data/demo.txt"));
             System.out.println("demo added to Redis");
-        }
-        catch (StorageException|IOException e)
-        {
+        } catch (IOException | StorageException e) {
             System.out.println("Error while reading files");
         }
     }
+
 }
